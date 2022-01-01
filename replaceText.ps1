b749171258b530921ae2f13668f2c7aba688360e
Elky (SeongHoon Kim) <elky84@gmail.com>
2022-01-01 17:50:48 +0900

    Change to record only the default version (1.0.0) in the csproj file and set the latest version as a build parameter

--- replaceText.ps1
+++ replaceText.ps1
@@ -1,8 +1,6 @@
 $PrevVersion='1.0.3'
 $NextVersion='1.0.4'
 
-((Get-Content -path ExcelToDotnet\ExcelToDotnet.csproj -Raw) -replace <Version>$PrevVersion</Version>,<Version>$NextVersion</Version>) | Set-Content -Path ExcelToDotnet\ExcelToDotnet.csproj
-
 ((Get-Content -path publish_to_github.bat -Raw) -replace $PrevVersion,$NextVersion) | Set-Content -Path publish_to_github.bat
 
 ((Get-Content -path publish_to_nuget.bat -Raw) -replace $PrevVersion,$NextVersion) | Set-Content -Path publish_to_nuget.bat
